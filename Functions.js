fcb80b43d0af0ae8635c7af11c8ada012bc4d93c
n-kalu <nnabugwuezema@gmail.com>
2022-06-10 02:50:54 -0600

    Created and renamed files and folder; Added the slideshow images and buttons

---
+++ Functions.js
@@ -0,0 +1,11 @@
+let m = document.getElementById("testing");
+
+m.addEventListener("click", myfunction);
+
+function myfunction(){
+    alert("You Just Clicked the Button");
+}
+
+function logoclick(){
+    alert("I just clicked my logo")
+}
